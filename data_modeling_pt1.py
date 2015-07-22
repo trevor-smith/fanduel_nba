6e5857f680d95d633db7920f8971b8a6b2f29735
trevor smith <trevor.stuard@gmail.com>
2015-07-21 22:05:05 -0700

    cleaned up coding

--- data_modeling_pt1.py
+++ data_modeling_pt1.py
@@ -1,5 +1,8 @@
 """
-The goal of this file is to document how I predict fanduel scores using only basic data from nbastuffer.com.  The data set has only been augmented slightly by changing column names to be more readable and I've also added in player positions which I found on nba.com
+The goal of this file is to document how I predict fanduel scores using only
+ basic data from nbastuffer.com.  The data set has only been augmented
+ slightly by changing column names to be more readable and I've also added in
+  player positions which I found on nba.com
 """
 
 ### IMPORTS ###
@@ -17,7 +20,8 @@ pd.set_option('display.max_columns', 100)
 %matplotlib inline
 
 # adding fanduel score
-df['fanduel_score'] = df.apply(lambda x: x['PTS'] + x['TOT']*1.2 + x['A']*1.5 + x['BL']*2 + x['ST']*2 -x['TO'], axis=1)
+df['fanduel_score'] = df.apply(lambda x: x['PTS'] + x['TOT']*1.2 + x['A']*1.5 +
+                               x['BL']*2 + x['ST']*2 -x['TO'], axis=1)
 
 # creating dummy columns
 home_dummies = pd.get_dummies(df.VENUE, prefix='venue')
@@ -28,7 +32,16 @@ team_dummies = pd.get_dummies(df.OPP_TEAM, prefix='Opponent')
 data = pd.concat([df, home_dummies, position_dummies, team_dummies], axis=1)
 
 # our features
-features = ['MIN', 'position_C', u'position_PF', u'position_PG', u'position_SF', u'position_SG', u'position_Unknown', 'TO', 'PF', 'venue_H', 'venue_R','Opponent_Bos', u'Opponent_Bro', u'Opponent_Cha', u'Opponent_Chi', u'Opponent_Cle', u'Opponent_Dal', u'Opponent_Den', u'Opponent_Det', u'Opponent_Gol', u'Opponent_Hou', u'Opponent_Ind', u'Opponent_Lac', u'Opponent_Lal', u'Opponent_Mem', u'Opponent_Mia', u'Opponent_Mil', u'Opponent_Min', u'Opponent_Nor', u'Opponent_Nyk', u'Opponent_Okc', u'Opponent_Orl', u'Opponent_Phi', u'Opponent_Pho', u'Opponent_Por', u'Opponent_Sac', u'Opponent_San', u'Opponent_Tor', u'Opponent_Uta', u'Opponent_Was']
+features = ['MIN', 'position_C', u'position_PF', u'position_PG', u'position_SF'
+, u'position_SG', u'position_Unknown', 'TO', 'PF', 'venue_H', 'venue_R'
+,'Opponent_Bos', u'Opponent_Bro', u'Opponent_Cha', u'Opponent_Chi'
+, u'Opponent_Cle', u'Opponent_Dal', u'Opponent_Den', u'Opponent_Det'
+, u'Opponent_Gol', u'Opponent_Hou', u'Opponent_Ind', u'Opponent_Lac'
+, u'Opponent_Lal', u'Opponent_Mem', u'Opponent_Mia', u'Opponent_Mil'
+, u'Opponent_Min', u'Opponent_Nor', u'Opponent_Nyk', u'Opponent_Okc'
+, u'Opponent_Orl', u'Opponent_Phi', u'Opponent_Pho', u'Opponent_Por'
+, u'Opponent_Sac', u'Opponent_San', u'Opponent_Tor', u'Opponent_Uta'
+, u'Opponent_Was']
 
 # split data into train and test
 train, test = train_test_split(data3, train_size = 0.8)
@@ -97,13 +110,16 @@ data3['past_10'] = pd.rolling_mean(data3['fanduel_score'], 10)
 
 train, test = train_test_split(data4, train_size = 0.8)
 
-features6 = ['past_1', 'past_3', 'past_5', 'past_10','MIN', 'position_C', u'position_PF', u'position_PG', u'position_SF', u'position_SG', u'position_Unknown',
-             'TO', 'PF', 'venue_H', 'venue_R','Opponent_Bos', u'Opponent_Bro', u'Opponent_Cha', u'Opponent_Chi',
-             u'Opponent_Cle',
-            u'Opponent_Dal', u'Opponent_Den', u'Opponent_Det', u'Opponent_Gol', u'Opponent_Hou', u'Opponent_Ind',
-            u'Opponent_Lac', u'Opponent_Lal', u'Opponent_Mem', u'Opponent_Mia', u'Opponent_Mil', u'Opponent_Min',
-            u'Opponent_Nor', u'Opponent_Nyk', u'Opponent_Okc', u'Opponent_Orl', u'Opponent_Phi', u'Opponent_Pho',
-            u'Opponent_Por', u'Opponent_Sac', u'Opponent_San', u'Opponent_Tor', u'Opponent_Uta', u'Opponent_Was']
+features6 = ['past_1', 'past_3', 'past_5', 'past_10','MIN', 'position_C',
+ u'position_PF', u'position_PG', u'position_SF', u'position_SG',
+ u'position_Unknown', 'TO', 'PF', 'venue_H', 'venue_R','Opponent_Bos',
+  u'Opponent_Bro', u'Opponent_Cha', u'Opponent_Chi',u'Opponent_Cle',
+  u'Opponent_Dal', u'Opponent_Den', u'Opponent_Det', u'Opponent_Gol',
+  u'Opponent_Hou', u'Opponent_Ind',u'Opponent_Lac', u'Opponent_Lal',
+  u'Opponent_Mem', u'Opponent_Mia', u'Opponent_Mil', u'Opponent_Min',
+  u'Opponent_Nor', u'Opponent_Nyk', u'Opponent_Okc', u'Opponent_Orl',
+   u'Opponent_Phi', u'Opponent_Pho',u'Opponent_Por', u'Opponent_Sac',
+   u'Opponent_San', u'Opponent_Tor', u'Opponent_Uta', u'Opponent_Was']
 
 x_train = train[features6]
 x_test = test[features6]
